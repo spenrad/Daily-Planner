4f6c422125a739b747361c643e1a46bcaa1c0be8
Spencer Christy <spenrad@gmail.com>
2021-01-04 22:38:05 -0800

    for loop created, all trs added to table via jquery

--- script.js
+++ script.js
@@ -9,16 +9,35 @@ var now = time.toLocaleString(DateTime.DATETIME_MED);
 
 $('h1').after(now);
 
+
+// for loop creates table rows corresponding with the hour "i"
+for (i=9; i < 18; i++) {
 // Create trs and tds with jquery
+  var tr = $('<tr>');
+  var timeSlot = $('<td>');
+  var textTd = $('<td>');
+  var textArea = $('<textarea>');
+  var saveIcon = $('<td>');
+    //sets the correct time for each tr
+  var timeStr = ''
+  if(i==12) {
+      timeStr= i + " p.m."
+  }
+    else if(i > 12) {
+      timeStr = i -12 + " p.m."
+    }   else {
+            timeStr = i + " a.m."
+        }
 
-var tr = $('<tr>');
-var timeSlot = $('<td>');
-var textTd = $('<td>');
-var textArea = $('<textarea>');
-var saveIcon = $('<td>');
-
-
-
-tr.append(timeSlot, textTd, saveIcon);
-textTd.append(textArea);
-$('#plannerTable').append(tr); 
+  timeSlot.text(timeStr);
+  textTd.addClass('input');
+  textArea.attr('id', i);
+  saveIcon.addClass('save');
+  saveIcon.text('save');
+  saveIcon.attr('name', i);
+  
+  tr.append(timeSlot, textTd, saveIcon);
+  textTd.append(textArea);
+  
+  $('#plannerTable').append(tr);  
+  } 
